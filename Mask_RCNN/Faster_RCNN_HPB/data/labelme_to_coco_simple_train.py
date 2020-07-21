1b00e13a7b4ccf8a56b8232a674ad965e793b57f
zhuyixintc <65342604+zhuyixintc@users.noreply.github.com>
2020-07-22 00:06:20 +0800

    Add files via upload

---
+++ Mask_RCNN/Faster_RCNN_HPB/data/labelme_to_coco_simple_train.py
@@ -0,0 +1,11 @@
+# import package
+import labelme2coco
+
+# set directory that contains labelme annotations and image files
+labelme_folder = 'C:/Users/yz18514/detectron2/Faster_RCNN_HPB/data/train_annotations'
+
+# set path for coco json to be saved
+save_json_path = 'C:/Users/yz18514/detectron2/Faster_RCNN_HPB/data/train.json'
+
+# conert labelme annotations to coco
+labelme2coco.convert(labelme_folder, save_json_path)
